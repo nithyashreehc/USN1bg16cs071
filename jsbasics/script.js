e41d59778a7f008ad256cc8523c4267a3dde4780
nithyashreehc <nithyahc1998@gmail.com>
2019-02-22 22:31:12 +0530

    HTML FILE3

---
+++ jsbasics/script.js
@@ -0,0 +1,81 @@
+//decision majking
+/*var age=18;
+if(age<18){
+    console.log("minor");
+}
+else if(age>=18){
+    console.log("major");
+}
+//con?true:false
+age>=18?console.log("major"): console.log("minor");*/
+//function
+/*function nithya(fname,lname)
+{
+    console.log(fname+''+lname);
+
+}
+
+nithya('shree','hc');*/
+//array
+/*var names=['nithya','ranjana','nidhi'];
+console.log(names);
+console.log(names.length);
+
+names.push("sri");
+
+names.unshift("prema");
+console.log(names);
+names.pop();
+names.push(2);
+console.log(names);*/
+
+
+var tips=[],bils=[];
+function calculatetips(bill)
+{
+    if(bill<50)
+    {
+    tip=bill*(20/100);
+}
+else if(bill<200 && bill>50)
+{
+    tip=bill*(15/100);
+
+}
+else{
+    tip=bill*(10/100);
+}
+tips.push(tip);
+bill=Number(bill)+Number(tip);
+bils.push(bill);
+}
+calculatetips('124');
+calculatetips('48');
+calculatetips('268');
+console.log(tips);
+console.log(bils);
+
+
+
+/*
+var mydetails={
+    fname:'nithya',lname:'shre',dob:1998,family:['nithya','nidhi'],
+    calcAge:function(birthYear){
+        return 2019-birthYear;
+    }
+};
+console.log(mydetails.fname);
+var z='dob';
+console.log(mydetails[z]);
+mydetails.dob=1999;
+console.log(mydetails);
+//new object
+var sri=new Object();
+sri.fname='prema';
+console.log(sri);
+
+//
+console.log('nithya is '+mydetails.calcAge(1998)+'years old');
+*/
+
+
